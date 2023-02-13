87616636eb4fd80746b04295317861f973b3a6b0
TymovanRijn <1057297@hr.nl>
2023-02-13 11:45:55 +0100

    Verbetering

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿/*
-return value from a method
-Write a program with a method called Pow2. This method takes an int as a parameter, and it returns the square. So for example: Pow2(5) should result in 25 as 5 * 5 is 25.
-
-Note: your submission will be unit tested. In order for the unit test to be able to test your program, you should make your class public.
-*/
-public class Program
+﻿public class Program
 {
     public static void Main()
     {
@@ -14,5 +8,4 @@ public class Program
     {
         return number * number;
     }
-    hallo
 }
