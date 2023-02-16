2a23863f270e3c059ff4985769e10f4f976284e8
TymovanRijn <1057297@hr.nl>
2023-02-16 20:33:49 +0100

    Test

---
+++ GameFiles/Program.cs
@@ -0,0 +1,10 @@
+// make an approachable main class
+public class Program
+{
+    // make a main method
+    public static void Main()
+    {
+        EndGame TheEnd = new EndGame();
+        TheEnd.End_The_Game();
+    }
+}
---
+++ GameFiles/Quest.cs
@@ -0,0 +1,37 @@
+public class Quest
+{
+    // public int Gathered = 0;
+    // public int Total;
+    // public string ItemName;
+    // public int Quest_ID;
+    public bool Quest1 = false;
+    public bool Quest2 = false;
+    public bool Quest3 = true;
+    public bool Quest4 = false;
+    //     public Quest(int quest_id, int total, string itemname)
+    //     {
+    //         this.Total = total;
+    //         this.ItemName = itemname;
+    //         this.Quest_ID = quest_id;
+    //     }
+
+    //     public string Completed()
+    //     {
+    //         if (Gathered >= Found)
+    //         {
+    //             return "You have completed the quest";
+    //         }
+    //         else
+    //         {
+    //             return $"you still need to find {Gathered}/{Total} items";
+    //         }
+
+    //     }
+
+    //     public int Found()
+    //     {
+    //         return Gathered++;
+    //     }
+
+
+}
