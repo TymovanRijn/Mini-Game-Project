5bab4d6ff683b1343ee75ea48d48711f08edcc08
SemLindeboom <1051855@hr.nl>
2023-02-20 08:58:17 +0100

    Fight

--- Game Files/Quest.cs
+++ Game Files/Quest.cs
@@ -8,6 +8,8 @@ public class Quest
     public int Quest_ID;//ID van de quest om makelijker te loopen indien nodig.
     public string NPC_Name;//NPC naam voor makelijker gespreken te maken.
     public int Item_Reward_ID;//Reward id zodat we de items in de inventory kunnen toevoegen.
+
+    public int health = 100; //De health van de speler.
     public Quest(int quest_id, int total_item, string itemname, string npc_name, int item_reward_id)
     {
         this.Total_Item = total_item;
@@ -15,6 +17,15 @@ public class Quest
         this.Quest_ID = quest_id;
         this.NPC_Name = npc_name;
         this.Item_Reward_ID = item_reward_id;
+    }
+
+    //reset de health bij een completed quest weer naar 100
+    public void gain_health()
+    {
+        if (Completed_Quest = true)
+        {
+            health = 100;
+        }
     }
 
     public string Completed()//Gebruik bij de npc om te kijken of hij het kan inleveren
